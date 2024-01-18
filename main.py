0b95dc6facbef3a2528b3e0c57b0e1fce9c6ed05
laundrevity <mahany25@gmail.com>
2024-01-18 16:45:05 -0500

    added support for floats and unsigned ints

--- main.py
+++ main.py
@@ -60,18 +60,24 @@ impl Header {
 
 """
 
+
 def get_test_value(rust_type: str) -> str:
-    if rust_type[0] in ('i', 'u'):
-        return '1234'
-    elif rust_type == 'bool':
-        return 'true'
-    elif rust_type.startswith('[char;'):
+    if rust_type[0] == "i":
+        return "-123"
+    elif rust_type[0] == "u":
+        return "123"
+    elif rust_type == "bool":
+        return "true"
+    elif rust_type.startswith("[char;"):
         return f"""string_to_char_array("John Doe").unwrap()"""
-    elif rust_type.startswith('Option'):
-        inner_type = rust_type[rust_type.index('<')+1:-1]
+    elif rust_type.startswith("Option"):
+        inner_type = rust_type[rust_type.index("<") + 1 : -1]
         return f"""Some({get_test_value(inner_type)})"""
+    elif rust_type[0] == "f":
+        return "3.14"
     else:
         raise Exception(f"Unknown Rust type for get_test_value: {rust_type}")
+
 
 def parse_xml_schema(xml_file: str):
     tree = ET.parse(xml_file)
@@ -110,11 +116,15 @@ def generate_rust_code_for_schema(message_formats) -> str:
             not attribute["required"],
         )
         inner_type = ""
-        if base_type == "integer":
+        if base_type == "int":
             inner_type = f"i{length * 8}"
-        elif base_type == "boolean":
+        elif base_type == "uint":
+            inner_type = f"u{length * 8}"
+        elif base_type == "float":
+            inner_type = f"f{length * 8}"
+        elif base_type == "bool":
             inner_type = "bool"
-        elif base_type == "string":
+        elif base_type == "str":
             inner_type = f"[char; {length}]"
 
         if optional:
@@ -123,22 +133,21 @@ def generate_rust_code_for_schema(message_formats) -> str:
             return inner_type
 
     def get_rust_num_bytes(rust_type: str) -> int:
-        if rust_type[0] in ('i', 'u'):
+        if rust_type[0] in ("i", "u", "f"):
             num_bits = int(rust_type[1:])
             return int(num_bits / 8)
-        elif rust_type.startswith('[char'):
+        elif rust_type.startswith("[char"):
             # technically char is 4 bytes but we are presupposing ASCII so its only 1
-            return int(rust_type[rust_type.index(';') + 1:-1])
-        elif rust_type == 'bool':
+            return int(rust_type[rust_type.index(";") + 1 : -1])
+        elif rust_type == "bool":
             return 1
-        elif rust_type.startswith('Option<'):
-            return get_rust_num_bytes(rust_type[rust_type.index('<') + 1:-1])
-
+        elif rust_type.startswith("Option<"):
+            return get_rust_num_bytes(rust_type[rust_type.index("<") + 1 : -1])
 
     def get_serialization_code(att, rust_type: str, omit_self=False) -> str:
         code = ""
-        if rust_type.startswith('Option<'):
-            inner_type = rust_type[rust_type.index('<')+1:-1]
+        if rust_type.startswith("Option<"):
+            inner_type = rust_type[rust_type.index("<") + 1 : -1]
             # [:-1] at end to exclude semicolon inside match arm
             return f"""\t\tmatch self.{att} {{\n\t\t\tSome({att}) => {get_serialization_code(att, inner_type, omit_self=True)[:-1]},\n\t\t\t_ => {{}}\n\t\t}}"""
         else:
@@ -149,11 +158,11 @@ def generate_rust_code_for_schema(message_formats) -> str:
                 self_string = ""
                 tab_string = ""
 
-            if rust_type[0] in ('i', 'u'):
+            if rust_type[0] in ("i", "u", "f"):
                 return f"""{tab_string}buf.extend_from_slice(&{self_string}{att}.to_be_bytes());"""
-            elif rust_type == 'bool':
+            elif rust_type == "bool":
                 return f"""{tab_string}buf.push(if {self_string}{att} {{ 1 }} else {{ 0 }});"""
-            elif rust_type.startswith('[char;'):
+            elif rust_type.startswith("[char;"):
                 return f"""{tab_string}buf.append({self_string}{att}.iter().map(|&c| c as u8).collect::<Vec<u8>>().as_mut());"""
 
         return code
@@ -162,10 +171,10 @@ def generate_rust_code_for_schema(message_formats) -> str:
         code = "\t\tlet mut mask: u32 = 0;\n\n"
         cnt = 0
         for att_name, rust_type in attribute_rust_types:
-            if rust_type.startswith('Option<'):
+            if rust_type.startswith("Option<"):
                 code += f"""\t\tmatch self.{att_name} {{\n\t\t\tSome(_) => mask |= 1 << {cnt},\n\t\t\t_ => {{}}\n\t\t}}\n\n"""
                 cnt += 1
-        
+
         code += f"""\t\tmask\n\t}}"""
         return code
 
@@ -182,14 +191,14 @@ def generate_rust_code_for_schema(message_formats) -> str:
 
         for att_name, rust_type in attribute_rust_types:
             ok_code += f"""\t\t\t{att_name},\n"""
-            if rust_type.startswith('Option'):
-                inner_rust_type = rust_type[rust_type.index('<')+1:-1]
+            if rust_type.startswith("Option"):
+                inner_rust_type = rust_type[rust_type.index("<") + 1 : -1]
                 n = get_rust_num_bytes(inner_rust_type)
 
                 code += f"""\t\tlet {att_name} = if header.bitmask & (1 << {opt_cnt}) != 0 {{\n"""
                 opt_cnt += 1
 
-                if inner_rust_type.startswith('[char;'):
+                if inner_rust_type.startswith("[char;"):
                     code += f"""\t\t\tlet mut {att_name}_chars = [' '; {n}];\n"""
                     code += f"""\t\t\tfor i in 0..{n} {{\n"""
                     code += f"""\t\t\t\t{att_name}_chars[i] = buffer[offset + i] as char;\n"""
@@ -197,51 +206,50 @@ def generate_rust_code_for_schema(message_formats) -> str:
 
                     code += f"""\t\t\toffset += {n};\n"""
                     code += f"""\t\t\tSome({att_name}_chars)\n"""
-            
-                elif inner_rust_type[0] in ('i', 'u'):
+
+                elif inner_rust_type[0] in ("i", "u", "f"):
                     code += f"""\t\t\tlet {att_name}_value = {inner_rust_type}::from_be_bytes(buffer[offset..offset + {n}].try_into().map_err(|_| "Invalid buffer: {att_name}")?);\n"""
-                
+
                     code += f"""\t\t\toffset += {n};\n"""
                     code += f"""\t\t\tSome({att_name}_value)\n"""
 
-                elif inner_rust_type[0] == 'bool':
+                elif inner_rust_type[0] == "bool":
                     code += f"""\t\t\tlet {att_name}_value = buffer[offset] != 0;"""
 
                     code += f"""\t\t\toffset += {n};\n"""
                     code += f"""\t\t\tSome({att_name}_value)\n"""
-                
+
                 code += f"""\t\t}} else {{\n\t\t\tNone\n\t\t}};\n\n"""
 
             else:
                 n = get_rust_num_bytes(rust_type)
 
-                if rust_type.startswith('[char;'):
+                if rust_type.startswith("[char;"):
                     code += f"""\t\tlet mut {att_name} = [' '; {n}];\n"""
                     code += f"""\t\tfor i in 0..{n} {{\n"""
                     code += f"""\t\t\t{att_name}[i] = buffer[offset + i] as char;\n"""
                     code += f"""\t\t}}\n"""
-                
-                elif rust_type[0] in ('i', 'u'):
+
+                elif rust_type[0] in ("i", "u", "f"):
                     code += f"""\t\t let {att_name} = {rust_type}::from_be_bytes(buffer[offset..offset + {n}].try_into().map_err(|_| "Invalid buffer: {att_name}")?);\n"""
 
-                elif rust_type == 'bool':
+                elif rust_type == "bool":
                     code += f"""\t\tlet {att_name} = buffer[offset] != 0;\n"""
 
                 code += f"""\t\toffset += {n};\n\n"""
 
         ok_code += f"""\t\t}})\n"""
-        
+
         code += ok_code
 
         code += f"""\t}}"""
         return code
 
-
     for message_format in schema:
         code += f"""#[derive(PartialEq, Debug)]\npub struct {message_format['name'].capitalize()} {{\n"""
         attribute_rust_types = []
         for attribute in message_format["attributes"]:
-            attribute_rust_types.append([attribute['name'], get_rust_type(attribute)])
+            attribute_rust_types.append([attribute["name"], get_rust_type(attribute)])
 
         for attribute, rust_type in attribute_rust_types:
             code += f"    pub {attribute}: {rust_type},\n"
@@ -267,14 +275,14 @@ def generate_rust_code_for_schema(message_formats) -> str:
 
         for att, rust_type in attribute_rust_types:
             code += f"{get_serialization_code(att, rust_type)}\n\n"
-        
+
         # end serialize
         code += """\n\t\tbuf\n\t}\n\n"""
 
         # get_bitmask
         code += f"""\tfn get_bitmask(&self) -> u32 {{\n"""
         code += f"""{get_bitmask_code(attribute_rust_types)}\n\n"""
-        
+
         code += f"""{get_deserialization_code(attribute_rust_types)}\n\n"""
 
         # end struct impl
@@ -301,15 +309,17 @@ def generate_rust_code_for_schema(message_formats) -> str:
     code += f"""\t\t\tmsg_size: buffer.len() as u32 + 9, // +9 for header size\n"""
     code += f"""\t\t\tmsg_type: match self {{\n"""
     for i, message_format in enumerate(schema):
-        code += f"""\t\t\t\tMessage::{message_format['name'].capitalize()}(_) => {i+1},\n"""
+        code += (
+            f"""\t\t\t\tMessage::{message_format['name'].capitalize()}(_) => {i+1},\n"""
+        )
     code += f"""\t\t\t}},\n"""
     code += f"""\t\t\tbitmask: self.get_bitmask(),\n"""
     code += f"""\t\t}};\n\n"""
-    
+
     code += f"""\t\tlet mut header_bytes = header.to_bytes().to_vec();\n"""
     code += f"""\t\theader_bytes.append(&mut buffer);\n"""
     code += f"""\t\theader_bytes\n"""
-    
+
     # end Message::serialize
     code += f"""\t}}\n\n"""
 
@@ -322,11 +332,15 @@ def generate_rust_code_for_schema(message_formats) -> str:
     code += f"""\t}}\n\n"""
 
     # begin Message::deserialize
-    code += f"""\tpub fn deserialize(buffer: &[u8]) -> Result<Self, &'static str> {{\n"""
+    code += (
+        f"""\tpub fn deserialize(buffer: &[u8]) -> Result<Self, &'static str> {{\n"""
+    )
     code += f"""\t\tmatch buffer[4] {{\n"""
     for i, message_format in enumerate(schema):
-        name = message_format['name']
-        code += f"""\t\t\t{i+1} => match {name.capitalize()}::deserialize(buffer) {{\n"""
+        name = message_format["name"]
+        code += (
+            f"""\t\t\t{i+1} => match {name.capitalize()}::deserialize(buffer) {{\n"""
+        )
         code += f"""\t\t\t\tOk({name}) => Ok(Message::{name.capitalize()}({name})),\n"""
         code += f"""\t\t\t\tErr(e) => Err(e),\n"""
         code += f"""\t\t\t}},\n"""
@@ -373,11 +387,11 @@ mod tests {
 """
     for message_format in schema:
         code += f"""\t#[test]\n"""
-        name = message_format['name']
+        name = message_format["name"]
         code += f"""\tfn test_{name}_serialize_deserialize() {{\n"""
 
         code += f"""\t\tlet message_original = Message::{name.capitalize()}({name.capitalize()} {{\n"""
-        for attrib in message_format['attributes']:
+        for attrib in message_format["attributes"]:
             print(f"{message_format=}, {attrib=}")
             code += f"""\t\t\t{attrib['name']}: {get_test_value(get_rust_type(attrib))},\n"""
         code += f"""\t\t}});\n\n"""
@@ -388,11 +402,9 @@ mod tests {
 
         code += f"""\t}}\n\n"""
 
-
     code += f"""}}\n"""
 
     return code
-
 
 
 if __name__ == "__main__":
